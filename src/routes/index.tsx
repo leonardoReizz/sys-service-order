226a92a1a769a6a640683c0a918072dc9cd45339
Leonardo Reis <amarantee.reis@gmail.com>
2023-03-01 21:52:23 -0300

    fix: remove context from routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ClientsContextProvider } from "../context/ClientsContext";
 import { ProductsContextProvider } from "../context/ProductsContext";
+import { Clients } from "../pages/Clients";
 import { Dashboard } from "../pages/Dashboard";
 import { Products } from "../pages/Products";
+import { ServiceOrder } from "../pages/ServiceOrder";
 import { LayoutWithSidebar } from "./LayoutWithSidebar";
-
-
 
 export function AppRoutes() {
   return (
@@ -12,7 +13,9 @@ export function AppRoutes() {
       <Routes>
         <Route element={<LayoutWithSidebar />} >
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/products" element={<ProductsContextProvider><Products /></ProductsContextProvider>} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/clients" element={<Clients />} />
+          <Route path="/serviceOrder" element={<ServiceOrder />} />
         </Route>
       </Routes>
     </BrowserRouter>
