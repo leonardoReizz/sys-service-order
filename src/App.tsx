4126441dad47450e103d1343b147f87059eb2585
Leonardo Reis <amarantee.reis@gmail.com>
2023-03-01 21:51:20 -0300

    feat: add Products and Clients Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "styled-components"
+import { ClientsContextProvider } from "./context/ClientsContext"
+import { ProductsContextProvider } from "./context/ProductsContext"
 import { AppRoutes } from "./routes"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
@@ -6,7 +8,11 @@ import { defaultTheme } from "./styles/themes/default"
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <AppRoutes />
+      <ProductsContextProvider>
+        <ClientsContextProvider>
+          <AppRoutes />
+        </ClientsContextProvider>
+      </ProductsContextProvider>
       <GlobalStyle />
     </ThemeProvider>
   )
